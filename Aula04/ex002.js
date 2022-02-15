45cc392ff42ad7e17b91b851cddedd5b20ab0bf0
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-15 19:53:06 -0300

    Exercício 2

---
+++ Aula04/ex002.js
@@ -0,0 +1,15 @@
+//Crie um arquivo chamado ex2.js
+
+//Declare, atribua e exiba 3 variáveis ​​por console: uma string, uma numérica e uma booleana. 
+
+let nomePet = "Tobias"
+let idadePet = 1 
+let petCastrado = true
+
+
+console.log("Nome do pet: ")
+console.log(nomePet)
+console.log("Idade do pet: ")
+console.log(idadePet)
+console.log("O pet é castrado? ")
+console.log(petCastrado)
