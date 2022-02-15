85584e219ce3a8fb4453ec56e85f19f9bfa5039f
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-15 20:33:49 -0300

    Excercício 3

---
+++ Aula04/ex003.js
@@ -0,0 +1,39 @@
+/*
+
+1) Em um novo arquivo chamado ex3.js, exiba na tela a soma de dois números.
+
+2) Repita o processo anterior, mas utilizando outro operador.
+
+3) Declare e atribua 2 variáveis ​​numéricas independentes. Exiba a soma de ambas na tela.
+
+4) Atribua a soma das duas variáveis ​​anteriores a uma outra variável e exiba-a na tela.
+
+5) Re-atribua à variável anterior "1" +1, e exiba na tela. (O que  aconteceu?)
+ 
+6) Re-atribua "olá" + 23, e exiba na tela. (O que aconteceu?)
+
+7) Crie mensagens concatenadas com valores diferentes.
+
+*/
+
+console.log(1+1)
+
+console.log(2*2)
+
+var num1 = 10;
+var num2 = 5;
+
+console.log(num1+num2);
+
+var novaSoma = num1+num2
+
+console.log(novaSoma)
+
+var novaSoma2 = novaSoma+1
+
+console.log(novaSoma2)
+
+var novaSoma3 = novaSoma2+23
+
+console.log(novaSoma3)
+
