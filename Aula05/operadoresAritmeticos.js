0691d88beebe97f899f04563d4774f6c10e7a5d8
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-15 21:14:25 -0300

    Fixação de operadores aritméticos

---
+++ Aula05/operadoresAritmeticos.js
@@ -0,0 +1,54 @@
+//Exemplos para fixação da máteria, nesse caso são de operação aritiméticas em js
+
+//adição
+
+console.log(2+2);
+
+//subtração
+
+console.log(1-1);
+
+//multiplicação
+
+console.log(3*3);
+
+//incremento
+
+var num1 = 10
+console.log(++num1);// Se colocado antes da variavel ele printa na hora no console, se colocado depois ele incrementa mas só imprime na próxima chamada.
+
+//decremento
+
+console.log(--num1);
+
+//comparação de maior
+
+console.log(10>5);
+
+//comparação de menor
+
+console.log(10<5);
+
+//comparação de menor ou igual
+
+console.log(3 <= 3);
+
+//comparação de maior ou igual
+
+console.log(20 >= 14);
+
+//comparação de igualdade
+
+console.log(10 == 5);
+
+//comparação de igualdade rigido
+
+console.log(5 === "5");
+
+//comparação de diferente
+
+console.log(10 != 10);
+
+//comparação de diferente rigido
+
+console.log(10 !== "10");
