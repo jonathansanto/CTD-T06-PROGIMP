efdd53e8a5ed749ca33b9df748764298122fdb9f
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-15 21:27:41 -0300

    Fixação de operadores lógicos

---
+++ Aula05/operadoresLogicos.js
@@ -0,0 +1,29 @@
+//Exemplos para fixação da máteria, nesse caso são de operadores lógicos 
+
+
+//& se um dos resultado for falso todo resultado será falso
+// OR ou ||  se um dos resultados for verdadeiro todo o resultado será verdadeiro
+// ! permite alterar o valor para verdadeiro ou falso
+// + é usado para unir duas ou mais cadeias de texto
+
+
+
+
+//comparador lógico &&
+
+console.log(10 > 5 && 8 > 10);
+
+//comparador lógico OR ||
+
+console.log(10 > 5 || 8 > 10);
+
+//comparador de negação !
+
+console.log(!5 > 3);
+
+//comparador de concatenação
+
+let nome = "João";
+let sobrenome = "Oliveira";
+
+console.log(nome + " " +  sobrenome);
