aa74704b952022eca0ba07e7840c94fdcbc85338
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-16 20:46:56 -0300

    Micro desafios sobre funções

---
+++ Aula05/microDesafio.js
@@ -0,0 +1,18 @@
+// O que cada função retorna? nesse exercício você terá que pensar sobre o que cada função retornará, sem executá-lá no console.
+
+function teste1(x,y){
+    return y - x;
+
+}
+
+teste1(10,40); // retornará undefined pois por mais que tenha colocado o valor na função não chamou ela para ser exibida no console.
+
+function teste2(x, y){
+    return x * 2
+    console.log(x)
+    return x / 2
+}
+
+teste2(10)// Não retornará nada pois quando o primeiro return é colocado ele *termina* a execução do bloco.
+
+
