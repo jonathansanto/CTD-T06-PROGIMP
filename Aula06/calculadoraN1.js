efdffd7ca3c285925922e6b8cba1a34e14ea32b3
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-18 13:16:20 -0300

    Calculadora nível 1

---
+++ Aula06/calculadoraN1.js
@@ -0,0 +1,36 @@
+
+//Crie uma função adicionar, que deverá receber dois parâmetros e retornar a soma deles.
+
+var num1 = 5
+var num2 = 10
+
+var resultado = num1 + num2
+
+console.log("A soma entre "+ num1 + " e " + num2 + " é " + resultado + "!");
+
+//Crie uma função de subtração, que deverá receber dois parâmetros e retornar a subtração do primeiro menos o segundo.
+
+var num3 = 10
+var num4 = 5
+
+var resultado1 = num3 - num4
+
+console.log("A subtração entre "+ num3 + " e " + num4 + " é " + resultado1 + "!");
+
+//Crie uma função de multiplicação, que deverá receber dois parâmetros e retornar o resultado de sua multiplicação.
+
+var num5 = 12
+var num6 = 7
+
+var resultado2 = num5 * num6
+
+console.log("A multiplicação entre " + num5 + " e " + num6 + " é " + resultado2 + "!")
+
+//Crie uma função de divisão, que receberá dois parâmetros e retornará o resultado da divisão do primeiro sobre o segundo.
+
+var num7 = 6
+var num8 = 3
+
+var resultado3 = num7 / num8
+
+console.log("A divisão de " + num7 + " para " + num8 + " é " + resultado3 + "!")
