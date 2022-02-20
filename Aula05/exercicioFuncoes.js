fc3d6fef5cc58bf7363953b65396526809193f32
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-19 23:33:24 -0300

    Correção

--- Aula05/exercicioFuncoes.js
+++ Aula05/exercicioFuncoes.js
@@ -47,7 +47,7 @@ function valorHrMes(salario){
 console.log(valorHrMes(1500));
 
 //Crie a função calculadora de IMC* de uma pessoa, que recebe a altura em centímetros e o peso em quilogramas. Em seguida, execute a função, testando diferentes valores. 
-//*IMC, significa Índice de Massa Corporal e é um parâmetro utilizado para saber se o peso está de acordo com a altura.*/
+//IMC, significa Índice de Massa Corporal e é um parâmetro utilizado para saber se o peso está de acordo com a altura.*/
 
 
 var peso = 80
