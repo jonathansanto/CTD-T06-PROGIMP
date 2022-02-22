b4dd946eace36ec6d686543d80f86f8f79f4bd95
Jonathan Santo <jonathanoesanto@outlook.com>
2022-02-21 22:21:27 -0300

    Calculadora nível 3

---
+++ Aula06/calculadoraN3.js
@@ -0,0 +1,26 @@
+/* 
+
+Crie uma função geradorDePorcentagem que leva dois parâmetros, e que deverá retornar a porcentagem do primeiro em relação ao segundo parâmetro.
+Exemplo: geradorDePorcentagem (2, 200) (deve retornar 1 já que 2 é 1% de 200). */
+
+//Crie uma função chamada quadradoDoNumero, que recebe um número como parâmetro e deve retornar esse número multiplicado por ele mesmo, ou seja, ao quadrado.
+//Importante: quadradoDoNumero() deve usar a função multiplicação() para calcular o quadrado do parâmetro inserido em power().
+
+function quadradoDoNumero(x){
+    return x * x;
+}
+
+console.log(quadradoDoNumero(8));
+
+//Crie a função mediaDeTresNumeros, ela deve calcular a média de 3 números, que serão inseridos por parâmetro.
+//Importante: Em mediaDeTresNumeros() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
+
+function mediaDeTresNumeros(a, y, z){
+    return (a + y + z) / 3;
+}
+
+console.log(mediaDeTresNumeros(5,4,3));
+
+
+
+
